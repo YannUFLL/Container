74829ff1ebb964100dadc75b3ac36f41662a8374
Yann Dumaine <yannlescoul79f>
2022-12-04 23:42:42 +0100

    Forget to save some header

--- stl_rewrite/lexicographical_compare.hpp
+++ stl_rewrite/lexicographical_compare.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexicographical_compare.hpp                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 23:34:56 by ydumaine          #+#    #+#             */
+/*   Updated: 2022/12/04 23:42:10 by ydumaine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LEXICOGRAPHICAL_COMPARE
 #define LEXICOGRAPHICAL_COMPARE
 namespace ft {
